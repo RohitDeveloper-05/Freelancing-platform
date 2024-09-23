f6e4504fd2758047f41635fb923572e9905109be
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-23 14:19:53 +0530

    code before scrolling effect 22.5

--- frontend/my-app/src/components/navbar/NavBar.jsx
+++ frontend/my-app/src/components/navbar/NavBar.jsx
@@ -4,7 +4,27 @@ import "./NavBar.scss"
 const NavBar = () =>{
   return(
     <div className='navbar'>
-      NavBar
+      <div className='container'>
+        <div className='logo'>
+            <span className='text'>fiverr</span>
+            <span className='dot'>.</span>
+        </div>
+
+        <div className="links">
+          <span>Fiver Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      
+      <hr/>
+      <div className="menu">
+        <span>Text 1</span>
+        <span>Text 2</span>
+      </div>
     </div>
   )
 }
