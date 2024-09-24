7cc4f3973f60eedd8936cabbf3c0315f47305e8d
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-24 18:16:30 +0530

    completed the first react infinite scroll bar by creating and modifying following components (CatCard,Slide,Home) )!)*%)

--- frontend/my-app/src/pages/home/Home.jsx
+++ frontend/my-app/src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import "./Home.scss"
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from '../../components/slide/Slide'
+import { cards } from '../../data'
+import CatCard from '../../components/catCard/CatCard'
 
 const Home = () =>{
   return(
@@ -9,6 +12,11 @@ const Home = () =>{
       Home
       <Featured />
       <TrustedBy/>
+      <Slide slidesToShow = {5} arrowsScroll = {3}>
+        {cards.map(card =>{
+          return <CatCard key={card.id} item={card}/>
+        })}
+      </Slide>
     </div>
   )
 }
