e7cd0db5fedb01bbe6a619d924a0c761cdf799d9
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-24 14:58:25 +0530

    Completed the Featured Component along with the proper designing(CSS part) %#$)

--- frontend/my-app/src/pages/home/Home.jsx
+++ frontend/my-app/src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./Home.scss"
+import Featured from '../../components/featured/Featured'
 
 const Home = () =>{
   return(
-    <div className='home'>Home</div>
+    <div className='home'>
+      Home
+      <Featured />
+    </div>
   )
 }
 
