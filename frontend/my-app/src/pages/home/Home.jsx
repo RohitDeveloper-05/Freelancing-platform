127486eac35919ece1747935f38593e8760f0656
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-26 14:10:49 +0530

    The second react infinite scroll bar on the Home Page is completed. About to start the Footer Section )!@^)$

--- frontend/my-app/src/pages/home/Home.jsx
+++ frontend/my-app/src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import "./Home.scss"
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
 import Slide from '../../components/slide/Slide'
-import { cards } from '../../data'
+import { cards,projects } from '../../data'
 import CatCard from '../../components/catCard/CatCard'
+import ProjectCard from '../../components/projectCard copy/ProjectCard'
 
 const Home = () =>{
   return(
@@ -93,6 +94,11 @@ const Home = () =>{
           </div>
         </div>
       </div>
+      <Slide slidesToShow = {4} arrowsScroll = {4}>
+        {projects.map(card =>{
+          return <ProjectCard key={card.id} item={card}/>
+        })}
+      </Slide>
     </div>
   )
 }
