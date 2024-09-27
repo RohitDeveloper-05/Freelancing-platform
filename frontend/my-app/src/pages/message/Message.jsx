a1eabd6a3c67dfdca8b641f20ecc92acae63a8f1
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-27 21:34:39 +0530

    Completed the Component Message(.i.e the React Chat Page which helps in chatting bw buyer & seller) )@$&#*

--- frontend/my-app/src/pages/message/Message.jsx
+++ frontend/my-app/src/pages/message/Message.jsx
@@ -1,10 +1,183 @@
-import React from 'react'
-import "./Message.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Message.scss";
 
-const Message = () =>{
-  return(
-    <div className='message'>Message</div>
-  )
-}
+const Message = () => {
+  return (
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">Messages</Link> > John Doe >
+        </span>
+        <div className="messages">
+          
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.               
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Yeah Sure, I can do that. I will deliver you the revised image in next 1-2 days. 
+              Kindly let me know incase if you want any other changesin the image
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Hey I wanted to make some revision in the image part of the image which you have generated.
+              Can you make the background of the image of be little dark and text to be little lighter. 
+              Plus By when can i expect the revised image.
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="write">
+          <textarea type="text" placeholder="write a message" />
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
