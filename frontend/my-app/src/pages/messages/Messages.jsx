3bfb18c14935aeaafc5453d74a14ab7ab899ffd1
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-27 20:59:59 +0530

    Completed the Component Messages (i.e.  It is meant for displaying all the messages).About to Start the React Chat Page Design

--- frontend/my-app/src/pages/messages/Messages.jsx
+++ frontend/my-app/src/pages/messages/Messages.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
-import "./Messages.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Messages.scss";
 
-const Messages = () =>{
-  return(
-    <div className='messages'>Messages</div>
-  )
-}
+const Messages = () => {
+  const currentUser = {
+    id: 1,
+    username: "Anna",
+    isSeller: true,
+  };
 
-export default Messages
+  const message = `Hey there, Thanks a lot for showing faith in getting services from us. Please feel
+  free to reach out to us, incase if you face any issue. Kindly please leave a review after the service completion`;
+
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>Charley Sharp</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 hours ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Elinor Good</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>Garner David </td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>2 days ago</td>
+          </tr>
+          <tr>
+            <td>Troy Oliver</td>
+            <td>{message.substring(0, 100)}</td>
+            <td>1 week ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
