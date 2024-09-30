072a44100f7ce8a7d96f65a03b262f96a3952136
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-30 14:04:58 +0530

    Just  added the router and controller files for each model without any content ##$%. ABout to start the Authentication Part.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
-import ConfigureDb from './Config/ConfigureDb.js'
 import expresss from 'express'
 import dotenv from 'dotenv'
+
+import ConfigureDb from './Config/ConfigureDb.js'
+
+import userRoute from "./App/routes/user.route.js"
+import gigRoute from "./App/routes/gig.route.js"
+import orderRoute from "./App/routes/order.route.js"
+import conversationRoute from "./App/routes/conversation.route.js"
+import messageRoute from "./App/routes/message.route.js"
+import reviewRoute from "./App/routes/review.route.js"
+
 
 const app = expresss()
 dotenv.config()
 
 ConfigureDb()
+
+app.use("/api/user",userRoute)
+app.use("/api/gigs",gigRoute)
+app.use("/api/orders",orderRoute)
+app.use("/api/conversations",conversationRoute)
+app.use("/api/messages",messageRoute)
+app.use("/api/reviews",reviewRoute)
+
 
 app.listen(8800, ()=>{
   console.log("Backend is Running")
