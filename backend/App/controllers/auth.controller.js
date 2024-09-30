1280da9f52b593936dae6ecb0efed9b0755881e5
rambo78-sensei <rohitsbelgaum@gmail.com>
2024-09-30 22:05:52 +0530

    Completed the logout functionality with fn login present inside the auth.controller.js )!!)$) About to start fn login using React application

--- backend/App/controllers/auth.controller.js
+++ backend/App/controllers/auth.controller.js
@@ -46,9 +46,11 @@ export const login = async (req,res,next) =>{
 }
 
 export const logout = async (req,res) =>{
-  try{
+  res.clearCookie("accessToken",{
+    sameSite:"none",
+    secure:true
+  })
 
-  }catch(err){
-    
-  }
+  .status(200)
+  .send("User has been logged out.");
 }
--- backend/App/routes/auth.route.js
+++ backend/App/routes/auth.route.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.post("/register",register)
 router.post("/login",login)
+router.get("/logout",logout)
 
 export default router
